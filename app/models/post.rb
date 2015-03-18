ed0d09c96cc7b0e743f4548677d0ca0789093af6
Emily Cunningham <ernoonan@gmail.com>
2015-03-18 17:14:21 +0100

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,9 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
+
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { reorder(title: :asc)}
+  scope :ordered_by_reverse_created_at, -> { reorder('created_at ASC')}
+
 end
