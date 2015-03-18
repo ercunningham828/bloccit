b6186d5f79f61ac7dbf8313f92060e705b5e5b66
Emily Cunningham <ernoonan@gmail.com>
2015-03-18 19:32:39 +0100

    Complete Authorization assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,4 +2,24 @@
   def index?
     true
   end
+
+  class Scope
+    attr_reader :user, :scope
+    def initialize(user,scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      unless user.present?
+        scope.none
+      else
+        if user.admin?
+          scope.all
+        else
+          scope.all.where(user_id: user.id)
+        end
+      end
+    end
+  end
  end
