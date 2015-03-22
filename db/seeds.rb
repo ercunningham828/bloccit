d32f43d526cc777f096989100b670ccf0a3b5e24
Emily Cunningham <ernoonan@gmail.com>
2015-03-22 18:42:57 +0100

    added pagination for topics and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ require 'faker'
  # The `save` method then saves this User to the database.
 
 # Create Topics
- 15.times do
+ 30.times do
    Topic.create!(
      name:         Faker::Lorem.sentence,
      description:  Faker::Lorem.paragraph
@@ -30,7 +30,7 @@ require 'faker'
  topics = Topic.all
 
  # Create Posts
- 50.times do
+ 300.times do
    Post.create!(
      user:   users.sample,
      topic:  topics.sample,
@@ -41,7 +41,7 @@ require 'faker'
  posts = Post.all
  
  # Create Comments
- 100.times do
+500.times do
    Comment.create!(
        # user: users.sample,   # we have not yet associated Users with Comments
      post: posts.sample,
