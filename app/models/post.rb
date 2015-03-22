0aaafe08d72cec569b37ce0142ac64703eaff09b
Emily Cunningham <ernoonan@gmail.com>
2015-03-22 13:52:37 +0100

    Complete post markdown assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,25 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+def render_as_markdown(markdown)
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {
+    fenced_code_blocks: true,
+    :no_intra_emphasis => true,
+    :autolink => true,
+    :strikethrough => true,
+    :lax_html_blocks => true,
+    :superscript => true,
+    :highlight => true,
+    :footnotes => true
+  }
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render markdown).html_safe
+  end
+  def markdown_body
+    render_as_markdown body
+  end
+  def markdown_title
+    render_as_markdown title
+  end
 end
