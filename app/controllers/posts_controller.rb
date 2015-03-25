b28597f9e46080d4d4c32be9244478b4c131b42d
Emily Cunningham <ernoonan@gmail.com>
2015-03-25 16:36:24 -0500

    completed destroy checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -55,4 +55,19 @@ def update
 def post_params
   params.require(:post).permit(:title, :body)
 end
+
+def destroy
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
+     title = @post.title
+     authorize @post
+ 
+     if @post.destroy
+       flash[:notice] = "\"#{title}\" was deleted successfully."
+       redirect_to @topic
+     else
+       flash[:error] = "There was an error deleting the post."
+       render :show
+     end
+   end
 end
