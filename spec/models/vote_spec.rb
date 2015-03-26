bbac3218a3252dfc2d22ebb6667be78ab99ba24c
Emily Cunningham <ernoonan@gmail.com>
2015-03-25 20:00:04 -0500

    Complete intro to rspec assignment

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,11 @@
+describe Vote do
+  describe "validations" do
+    before do
+      @vote=Vote.create(value: 1)
+    end
+  describe "value validation" do
+    it "only allows -1 or 1 as values" do
+      expect(@vote.value).to eq(-1 || 1)
+    end
+  end
+end
