4fd091426b7570de06066b51eab1dcd7f948d83e
Emily Cunningham <ernoonan@gmail.com>
2015-03-31 10:59:51 +0530

    Completed public profiles assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -7,7 +7,7 @@ require 'rails_helper'
    before do
      @user = authenticated_user
      @post = associated_post(user: @user)
-     @comment = Comment.new(user: @user, body: "A Comment")
+     @comment = Comment.new(user: @user, body: "A Comment", post: @post)
      allow(@comment).to receive(:send_favorite_emails)
      @comment.save
    end
@@ -24,5 +24,22 @@ require 'rails_helper'
       expect( page ).to have_content(@comment.body)
      end
  
+   end
+
+   describe "user visiting own profile" do
+
+      before do
+        login_as(@user, :scope => :user)
+      end
+ 
+     it "shows user their profile" do
+       visit user_path(@user)
+       expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+      end
+      Warden.test_reset! 
    end
  end
